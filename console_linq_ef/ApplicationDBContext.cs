3037cf9dedc3a0cb7153b7eac5dc6b58d72df89a
randallchacon <info@randallchacon.com>
2022-06-04 12:32:54 -0600

    Working with EF and SQLite

---
+++ console_linq_ef/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_linq_ef
+{
+    public class ApplicationDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data source=dbLinq.db");
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- console_linq_ef/Program.cs
+++ console_linq_ef/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using console_linq_ef;
+//First data
+/* 
+using(var context = new ApplicationDBContext())
+{
+    var person1 = new Person() { Name="Randall"};
+    var person2 = new Person() { Name = "Luis" };
+
+    context.AddRange(person1, person2);
+    context.SaveChanges();
+}
+*/
+
+using (var context = new ApplicationDBContext())
+{
+    var people = context.People.ToList();
+    var peopleName = context.People.Select(x => x.Name).ToList();//EF convert this to SELECT Name FROM PEOPLE;
+    var randall = context.People.FirstOrDefault(p => p.Name == "Randall");
+    var jose = context.People.Where(p => p.Name == "Jose").ToList();
+    var jose2 = context.People.FirstOrDefault(p => p.Name == "Jose");
+
+    //Query syntax
+    var randall_2 = (from p in context.People
+                    where p.Name == "Randall"
+                    select p).FirstOrDefault();
+
+    //Not all LINQ Expressions are valid in EF, for example Chunk doesn't exists
+    //var segments = context.People.Chunk(2).ToList();
+}
+
 Console.WriteLine("Hello, World!");
